4b5c244d00c1494c49df2233d65bea5d6617e5c7
Priyanshu8282 <priyanshus8282@gmail.com>
2026-03-04 16:22:53 +0530

    fix: Update environment variables in .env file for production API and image URLs

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    eslint: {
-        // Disable ESLint during production builds
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        // Disable type checking during production builds
-        ignoreBuildErrors: true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '5001',
-                pathname: '/uploads/**',
-            },
-        ],
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5001",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "fashion-flesta-backend-91rm.onrender.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
